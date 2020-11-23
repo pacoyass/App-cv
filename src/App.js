680e3f33bb933c158bf22dc5ca8cd60b1729f5c5
pacoyass <pacoyasix@gmail.com>
2020-11-23 12:35:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {BrowserRouter as Router,Switch,Route}from 'react-router-dom'
+import { DataCenter } from './ContextApi/DataCenter'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <DataCenter>
+        <Router>
+          <Switch>
+            
+          </Switch>
+        </Router>
+      </DataCenter>
     </div>
   );
 }
