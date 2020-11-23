9dee76ac0fc7665c1115260c876e20f5382dea0d
pacoyass <pacoyasix@gmail.com>
2020-11-23 15:45:43 +0100

    yassine

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import {BrowserRouter as Router,Switch,Route}from 'react-router-dom'
 import { DataCenter } from './ContextApi/DataCenter'
+import AddExperience from './dashbord/AddExperience';
+import Addformation from './dashbord/Addformation';
+import Addloisirs from './dashbord/Addloisirs';
+import Addprofile from './dashbord/Addprofile';
+import { Dashbord } from './ContextApi/Dashbord';
+
 
 
 function App() {
   return (
     <div className="App">
-      <DataCenter>
+      <Dashbord>
         <Router>
           <Switch>
-            <h1>hello guy's</h1>
+           <Route exact path="/addexperience" component={AddExperience}/> 
+           <Route exact path="/addformation" component={Addformation}/>  
+           <Route exact path="/addloisir" component={Addloisirs}/> 
+           <Route exact path="/addprofile" component={Addprofile}/> 
           </Switch>
         </Router>
-      </DataCenter>
+        </Dashbord>
     </div>
   );
 }
---
+++ src/dashbord/Addformation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Addformation() {
+    return (
+        <div>
+            <h1>hello adformation</h1>
+        </div>
+    )
+}
