b2341900b5be95d8bf56ac265d7a03e3721ef594
khalilPP12 <khalilrajachadery@gmail.com>
2020-11-24 09:33:50 +0100

    second project

--- src/dashbord/Addformation.jsx
+++ src/dashbord/Addformation.jsx
@@ -4,10 +4,10 @@ export default function Addformation() {
     return (
         <div>
         < div className='form-control col-md-6 offset-3 mt-5'>
-        {/* <input  name='name'  defaultValue={product.name} type="text" className='form-control'/>
-        <input  name='price'  defaultValue={product.price} type="text" className='form-control'/>
-        <input  name='stock_items'  defaultValue={product.stock_items} type="text" className='form-control'/>
-        <button className="btn btn-warning ">Update User</button> */}
+        <input  name='name'  type="text" className='form-control'/>
+        <input  name='price'   type="text" className='form-control'/>
+        <input  name='stock_items'  type="text" className='form-control'/>
+        <button className="btn btn-warning ">Update User</button>
     </div>
         
         <div>
