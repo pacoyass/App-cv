edeab2ca7ae24e07c958fc9e7ff309d7e15299d3
pacoyass <pacoyasix@gmail.com>
2020-11-24 11:49:46 +0100

    yassine

--- src/App.js
+++ src/App.js
@@ -8,19 +8,20 @@ import { Datacenter1 } from './ContextApi/Datacenter1';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Menu from './dashrss/Menu'
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
 import {Datacenter2} from './ContextApi/Datacenter2';
 import {Datacenter3} from './ContextApi/Datacenter3';
-
-=======
 import AddCompetence from './dashbord/AddCompetence';
->>>>>>> 2128111c947042650b62f22533294e24467fe3d3
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App  ">
+     
+
+      
+    
+      
       <Datacenter1>
        
          <Datacenter2>
@@ -28,15 +29,6 @@ function App() {
         <Router>
            <Menu/>
           <Switch>
-<<<<<<< HEAD
-           
-           <Route exact path="/addexperience" component={AddExperience}/> 
-           <Route exact path="/addformation" component={Addformation}/>  
-           <Route exact path="/addloisir" component={Addloisirs}/> 
-           <Route exact path="/addprofile" component={Addprofile}/> 
-  
-          </Switch>
-=======
            <Route exact path="/AddExperience" component={AddExperience}/> 
            <Route exact path="/Addformation" component={Addformation}/>  
            <Route exact path="/Addloisir" component={Addloisirs}/> 
@@ -44,12 +36,13 @@ function App() {
            <Route exact path="/AddCompetence" component={AddCompetence}/> 
            <Route exact path="/dashbord" component={Menu}/>
            </Switch>
->>>>>>> 2128111c947042650b62f22533294e24467fe3d3
         </Router>
          </Datacenter3>
         </Datacenter2> 
         </Datacenter1>
     </div>
+    
+    
   );
 }
 
--- src/dashrss/Menu.jsx
+++ src/dashrss/Menu.jsx
@@ -1,49 +1,96 @@
 import React from 'react';
-import './Dash.css';
-import { Grid, Cell } from 'react-mdl';
+import './simple.css'
+
 import { Link } from 'react-router-dom';
 
 export default function Menu() {
     return (
-        <div className="home ">
-        <div className="container-fluid ">
-          <div className="row ">
-            <div className="col-md-2 col-sm-1 hidden-xs" id="navigation">
-              
 
-              <div className="navi ">
-                <ul >
-                  <li><a href="#"><i className="fa fa-home" aria-hidden="true" /><span className="hidden-xs hidden-sm">Home</span></a></li>
-                  <li><a href="#"><i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Workflow</span></a></li>
-                  <li><a href="#"><i className="fa fa-bar-chart" aria-hidden="true" /><span className="hidden-xs hidden-sm">Statistics</span></a></li>
-                  <li><a href="#"><i className="fa fa-user" aria-hidden="true" /><span className="hidden-xs hidden-sm">Calender</span></a></li>
-                  <li><a href="#"><i className="fa fa-calendar" aria-hidden="true" /><span className="hidden-xs hidden-sm">Users</span></a></li>
-                  <li><a href="#"><i className="fa fa-cog" aria-hidden="true" /><span className="hidden-xs hidden-sm">Setting</span></a></li>
 
-              <div className="navi">
-                <ul>  
-                  <li className="active"><a href="#"><i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm text-white">Dashboard</span></a></li>
-                  <Link to="/Addformation"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Formation</span></Link>
-                  <Link to="/AddExperience"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Experience</span></Link>
-                   <Link to="/AddCompetence"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Competence</span></Link>
-                   <Link to="/addloisir"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Loisir</span></Link>
-                   <Link to="/addprofile"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Profile</span></Link>
-
-                </ul>
-              </div>
-            </div>
-            <div className="col-md-10 col-sm-11 display-table-cell v-align">
-         
-        
-              
-                 
-            </div>
-          </div>
+      <div className="d-flex " id="wrapper">
+      {/* Sidebar */}
+      <div className="bg-light border-right " id="sidebar-wrapper">
+        <div className="sidebar-heading">Start Bootstrap </div>
+        <div className="list-group list-group-flush ">
+          <a href="/Addprofile" className="list-group-item list-group-item-action bg-light">Dashboard</a>
+          <a href="#" className="list-group-item list-group-item-action bg-light">Add </a>
+          <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
+          <a href="#" className="list-group-item list-group-item-action bg-light">Events</a>
+          <a href="#" className="list-group-item list-group-item-action bg-light">Profile</a>
+          <a href="#" className="list-group-item list-group-item-action bg-light">Status</a>
         </div>
+      </div>
+      {/* /#sidebar-wrapper */}
+      {/* Page Content */}
+      {/* <div  id="page-content-wrapper">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+          
+        
+            
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <div className="dropdown-divider" />
+                  <a className="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        
+      </div> */}
+      {/* /#page-content-wrapper */}
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      //   <div className="home ">
+      //   <div className="container-fluid ">
+      //     <div className="row ">
+      //       <div className="col-md-2 col-sm-1 hidden-xs" id="navigation">
+              
+      //         <div className="navi">
+      //           <ul>  
+      //             <li className="active"><a href="#"><i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm text-white">Dashboard</span></a></li>
+      //             <Link to="/Addformation"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Formation</span></Link>
+      //             <Link to="/AddExperience"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Experience</span></Link>
+      //              <Link to="/AddCompetence"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Competence</span></Link>
+      //              <Link to="/addloisir"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Loisir</span></Link>
+      //              <Link to="/addprofile"> <i className="fa fa-tasks" aria-hidden="true" /><span className="hidden-xs hidden-sm">Add Profile</span></Link>
+
+      //           </ul>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
         
     
        
-      </div>
+      // </div>
 
 
 
